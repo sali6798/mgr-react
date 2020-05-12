3d4143de8516c9374e307d77355c797e0a173f99
sali6798 <shaidee@alingcastre.net>
2020-05-12 08:16:22 -0700

    router setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Switch>
+          {/* <Route exact path={["/", "/books"]} component={Books} /> */}
+          {/* <Route exact path="/books/:id" component={Detail} /> */}
+          {/* <Route exact path="*" component={NoMatch} /> */}
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
