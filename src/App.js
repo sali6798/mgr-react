0e43bd046a9ed489a010506d95a30d16080c1adb
bbtwots <brettbelka@gmail.com>
2020-05-12 20:45:24 -0700

    almost there

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import HomePage from './pages/Homepage'
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Navbar />
-        <Switch>
+        {/* <Switch> */}
           {/* <Route exact path={["/", "/books"]} component={Books} /> */}
           {/* <Route exact path="/books/:id" component={Detail} /> */}
           {/* <Route exact path="*" component={NoMatch} /> */}
-        </Switch>
+        {/* </Switch> */}
+        <Route exact path ="/" component={HomePage}/>
       </div>
     </BrowserRouter>
   );
