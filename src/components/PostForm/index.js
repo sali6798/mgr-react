0393926b8682b650b247724e9a57c5c076227d3a
sali6798 <shaidee@alingcastre.net>
2020-05-15 12:58:20 -0700

    form fields for post creation

--- src/components/PostForm/index.js
+++ src/components/PostForm/index.js
@@ -1,74 +1,145 @@
-import React from "react";
-import { TextField, TextareaAutosize } from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
-// import DateFnsUtils from '@date-io/date-fns';
-import moment from "moment";
-import MomentUtils from '@date-io/moment';
+import React, { useState } from 'react';
 import {
-    MuiPickersUtilsProvider,
-    KeyboardTimePicker,
-    KeyboardDatePicker,
-} from '@material-ui/pickers';
+    Button,
+    FormControl,
+    FormControlLabel,
+    FormGroup,
+    FormLabel,
+    Grid,
+    Radio,
+    RadioGroup,
+    Switch,
+    TextField,
+    List
+} from '@material-ui/core';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import DateTimePicker from "../DateTimePicker";
+import Item from "../Item";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
-            width: '25ch',
-        },
-    },
-}));
 
 function PostForm() {
-    const classes = useStyles();
-    const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+    // The first commit of Material-UI
+
+    const [allDay, setAllDay] = useState(false);
+    const [releaseStatus, setReleaseStatus] = useState("draft");
+    const [imgPath, setImgPath] = useState([]);
+    const [uploads, setUploads] = useState([]);
+    const [selectedDate, setSelectedDate] = useState(new Date());
 
     const handleDateChange = (date) => {
+        console.log(date)
         setSelectedDate(date);
     };
+
+    const handleChange = event => {
+        // console.log(selectedDate)
+        setAllDay(event.target.checked);
+    };
+
+    const handleClick = event => {
+        setReleaseStatus(event.target.value);
+    }
+
+    const uploadIMG = async () => {
+        // make object
+        const data = new FormData();
+        data.append("file", imgPath);         // GET Value from state >> upload
+
+        data.append("upload_preset", "udzc5qvv");
+        // upload file
+        const res = await fetch(
+            "https://api.cloudinary.com/v1_1/mgr/image/upload",
+            {
+                method: "POST",
+                body: data,
+            }
+        );
+        const file = await res.json();           // URL Link to picture
+        // console.log(file)
+        setUploads(uploads.concat({ 
+            id: imgPath.name + uploads.length,
+            name: imgPath.name,
+            url: file.url
+        }))
+    }
+
+    const selectFile = (e) => {
+        let file = e.target.files[0];
+        setImgPath(file);       // Set file to state
+    }
+
+
+    const displayScheduler = () => {
+        return (
+            <div>
+                <FormGroup >
+                    <FormControlLabel
+                        control={
+                            <Switch
+                                checked={allDay}
+                                onChange={handleChange}
+                                name="allDay"
+                                color="primary"
+                            />
+                        }
+                        label="All Day"
+                    />
+                </FormGroup>
+                <DateTimePicker selectedDate={selectedDate} handleDateChange={handleDateChange} allDay={allDay} />
+            </div>
+        );
+    }
+
+    const handleDelete = id => {
+        const newUploads = uploads.filter(upload => upload.id !== id);
+        setUploads(newUploads);
+    }
+
     return (
-        <div>
-            <form className={classes.root} noValidate autoComplete="off">
-                <TextField id="outlined-basic" label="Title" variant="outlined"></TextField>
-                <TextareaAutosize aria-label="minimum height" rowsMin={5} placeholder="Post Body" />
-            </form>
-            <MuiPickersUtilsProvider utils={MomentUtils} moment={moment}>
-                <KeyboardDatePicker
-                    disableToolbar
-                    variant="inline"
-                    format={moment(selectedDate).format("MMM Do YY") }
-                    margin="normal"
-                    id="date-picker-inline"
-                    label="Date picker inline"
-                    value={selectedDate}
-                    onChange={handleDateChange}
-                    KeyboardButtonProps={{
-                        'aria-label': 'change date',
-                    }}
+        <form noValidate autoComplete="off">
+            <Grid
+                container
+                direction="column"
+                justify="space-around"
+                alignItems="center"
+            >
+
+                <TextField label="Title" variant="outlined"></TextField>
+
+                <TextField
+                    id="outlined-multiline-static"
+                    label="Post Body"
+                    multiline
+                    rows={4}
+                    variant="outlined"
                 />
-                {/* <KeyboardDatePicker
-                    margin="normal"
-                    id="date-picker-dialog"
-                    label="Date picker dialog"
-                    format="MM/dd/yyyy"
-                    value={selectedDate}
-                    onChange={handleDateChange}
-                    KeyboardButtonProps={{
-                        'aria-label': 'change date',
-                    }}
-                />
-                <KeyboardTimePicker
-                    margin="normal"
-                    id="time-picker"
-                    label="Time picker"
-                    value={selectedDate}
-                    onChange={handleDateChange}
-                    KeyboardButtonProps={{
-                        'aria-label': 'change time',
-                    }}
-                /> */}
-            </MuiPickersUtilsProvider>
-        </div>
+                <Grid container alignItems="center" justify="center">
+                    <input type="file" id="uploadImg" name="uploadImg" onChange={selectFile} />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        startIcon={<CloudUploadIcon />}
+                        onClick={uploadIMG}
+                    >
+                        Upload
+                    </Button>
+                </Grid>
+                <List>
+                    {uploads.map(upload => <Item key={upload.id} {...upload} handleDelete={handleDelete} />)}
+                </List>
+
+                <FormControl component="fieldset">
+                    <FormLabel component="legend">Schedule Release</FormLabel>
+                    <RadioGroup row aria-label="position" name="position" defaultValue="draft" >
+                        <FormControlLabel value="draft" control={<Radio color="primary" onClick={handleClick} />} label="Not Ready" />
+                        <FormControlLabel value="ready" control={<Radio color="primary" onClick={handleClick} />} label="Send out now" />
+                        <FormControlLabel value="later" control={<Radio color="primary" onClick={handleClick} />} label="Schedule for later" />
+                    </RadioGroup>
+                </FormControl>
+
+                {releaseStatus === "later" ? displayScheduler() : ""}
+            </Grid>
+        </form>
     );
 }
 
