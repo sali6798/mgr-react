5e33655fd2f4a568785fff37d6d07e6e5f8a739b
sali6798 <shaidee@alingcastre.net>
2020-05-16 09:52:44 -0700

    different props for different labels

--- src/components/DatePicker/index.js
+++ src/components/DatePicker/index.js
@@ -2,13 +2,23 @@ import React from "react";
 import { KeyboardDatePicker } from '@material-ui/pickers';
 
 function DatePicker(props) {
+    function dateType() {
+        switch(props.dateLabel) {
+            case "Send Date":
+                return props.selectedDate;
+            case "Start Date":
+                return props.startDate;
+            default:
+                return props.endDate;
+        }
+    }
+
     return (
         <KeyboardDatePicker
             margin="normal"
-            // id="date-picker-dialog"
-            label="Date"
-            format="DD/MM/YYYY"
-            value={props.selectedDate}
+            label={props.dateLabel}
+            format="MM/DD/YYYY"
+            value={dateType()}
             onChange={props.handleDateChange}
             KeyboardButtonProps={{
                 'aria-label': 'change date',
