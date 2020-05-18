bdfed21ebf01797bb84eeeade84968ed31063714
sali6798 <shaidee@alingcastre.net>
2020-05-17 20:35:07 -0700

    search form to add users to group

---
+++ src/components/Search/index.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { TextField, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import API from "../../utils/API";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: 500,
+        '& > * + *': {
+            marginTop: theme.spacing(3),
+        },
+    },
+}));
+
+function Search(props) {
+    const classes = useStyles();
+    const [users, setUsers] = useState([])
+    const [artists, setArtists] = React.useState([]);
+
+    useEffect(() => {
+        API.getAllUsers()
+            .then(({ data }) => setUsers(data))
+            .catch(err => console.log(err))
+    }, [])
+
+    const handleOnChange = (event, newValue) => {
+        console.log(newValue)
+        setArtists(newValue);
+    }
+
+    const handleSubmit = () => {
+        if (artists.length > 0) {
+            API.addGroupArtist({
+                groupId: props.groupId,
+                artists: artists
+            })
+            .then(() => {
+                props.loadArtists();
+            })
+            .catch(err => console.log(err))
+        }
+        props.handleClose();
+    }
+
+    return (
+        <div className={classes.root}>
+            <Autocomplete
+                multiple
+                id="tags-outlined"
+                options={users}
+                onChange={handleOnChange}
+                getOptionLabel={(option) => option.email}
+                filterSelectedOptions
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        variant="outlined"
+                        label="filterSelectedOptions"
+                        placeholder="Add Artist"
+                    />
+                )}
+            />
+            <Button variant="contained" color="primary" onClick={handleSubmit}>
+                Add Artists
+            </Button>
+        </div>
+    );
+}
+
+export default Search;
